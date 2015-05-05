8c4d939235428f435bb014d7135a125b2cde256d
Andrew Dinh <andrewdinh74@yahoo.com>
2015-05-05 11:51:33 -0700

    Get to add food

--- client/models/homework.js
+++ client/models/homework.js
@@ -1,29 +1,29 @@
 'use strict';
 
 angular.module('homework')
-.factory('Grocery', function($http, nodeUrl){
+.factory('Homework', function($http, nodeUrl){
 
   function Homework(){
   }
 
-  Homework.getFoodList = function(){
-   return $http.get(nodeUrl + '/foods');
+  Homework.getHomeworkList = function(){
+   return $http.get(nodeUrl + '/homeworks');
   };
 
-  Homework.add = function(food){
-   return $http.post(nodeUrl + '/foods', food);
+  Homework.add = function(homework){
+   return $http.post(nodeUrl + '/homeworks', homework);
   };
-
-  Homework.save = function(food){
-   return $http.put(nodeUrl + '/foods/' + food._id, food);
-  };
-  // Grocery.toggle = function(food){
+  //
+  // Homework.save = function(food){
   //  return $http.put(nodeUrl + '/foods/' + food._id, food);
   // };
-
-  Homework.destroy = function(food){
-   return $http.delete(nodeUrl + '/foods/' + food._id);
-  };
+  // // Grocery.toggle = function(food){
+  // //  return $http.put(nodeUrl + '/foods/' + food._id, food);
+  // // };
+  //
+  // Homework.destroy = function(food){
+  //  return $http.delete(nodeUrl + '/foods/' + food._id);
+  // };
 
   return Homework;
 });
--- client/views/homeworks/homeworks.js
+++ client/views/homeworks/homeworks.js
@@ -3,70 +3,67 @@
 angular.module('homework')
 .controller('HomeworkCtrl', function($window, $scope, Homework){
 
-  $scope.food = {};
+  $scope.homework = {};
   $scope.editing = false;
   $scope.class = 'notPicked';
 
-  Homework.getFoodList()
+  Homework.getHomeworkList()
   .then(function(response){
-   $scope.foods = response.data.foods;
+   $scope.homeworks = response.data.homeworks;
   });
+  //
+  // $scope.destroy = function(food){
+  //  Homework.destroy(food)
+  //  .then(function(){
+  //   $window._.remove($scope.foods, function(f){
+  //    return f._id === food._id;
+  //   });
+  //  });
+  // };
 
-  $scope.destroy = function(food){
-   Homework.destroy(food)
-   .then(function(){
-    $window._.remove($scope.foods, function(f){
-     return f._id === food._id;
-    });
-   });
-  };
-
-  $scope.add = function(food){
-    Homework.add(food)
+  $scope.add = function(homework){
+    Homework.add(homework)
     .then(function(response){
-      $scope.foods.push(response.data);
-      $scope.food = {};
-      $scope.food.photo = {};
-      $window.Webcam.reset();
-
+      $scope.homeworks.push(response.data);
+      $scope.homework = {};
     })
     .catch(function(){
-      $window.swal({title: 'Error', text: 'There was a problem adding your food item. Please try again.', type: 'error'});
+      $window.swal({title: 'Error', text: 'There was a problem adding your homework item. Please try again.', type: 'error'});
     });
   };
 
-  $scope.editFood = function(food){
-   $scope.editing = true;
-   $scope.food = food;
-   Homework.getFoodList()
-   .then(function(foodList){
-    $scope.foods = foodList.data.foods;
-   });
-  };
-
-  $scope.toggleCheckbox = function(food){
-   Grocery.save(food)
-   .then(function(){
-    if ($scope.class === 'notPicked'){
-     $scope.class = 'picked';
-    }else{
-     $scope.class = 'notPicked';
-    }
-   });
-  };
-
-  $scope.save = function(food){
-   Grocery.save(food)
-   .then(function(){
-    $scope.food = {};
-    $scope.food.photo = {};
-    $window.Webcam.reset();
-    $scope.editing = false;
-    Grocery.getFoodList()
-    .then(function(foodList){
-     $scope.foods = foodList.data.foods;
-    });
-   });
-  };
+  // $scope.editFood = function(food){
+  //  $scope.editing = true;
+  //  $scope.food = food;
+  //  Homework.getFoodList()
+  //  .then(function(foodList){
+  //   $scope.foods = foodList.data.foods;
+  //  });
+  // };
+  //
+  // $scope.toggleCheckbox = function(food){
+  //  Grocery.save(food)
+  //  .then(function(){
+  //   if ($scope.class === 'notPicked'){
+  //    $scope.class = 'picked';
+  //   }else{
+  //    $scope.class = 'notPicked';
+  //   }
+  //  });
+  // };
+  //
+  // $scope.save = function(food){
+  //  Grocery.save(food)
+  //  .then(function(){
+  //   $scope.food = {};
+  //   $scope.food.photo = {};
+  //   $window.Webcam.reset();
+  //   $scope.editing = false;
+  //   Grocery.getFoodList()
+  //   .then(function(foodList){
+  //    $scope.foods = foodList.data.foods;
+  //   });
+  //  });
+  // };
 
 });
